eaf2d41b9108f5c94943a1f80b79332cdeda462f
Amesh Alex Kuruvilla <finixbenny@Ameshs-MacBook-Air.local>
2020-09-29 12:46:26 -0500

    wrong commit

--- daywise_work/day12_CovidMapperAuthentication/covidmapper/covidmapper_api/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do    
-  root 'welcome#index'                                            
-  resources :users do                                                            
-    collection do                                                                
-      post '/login', to: 'users#login' 
-      post '/users', to: 'users#create'                                               
-    end                                                                          
-  end                                                                                
-end
