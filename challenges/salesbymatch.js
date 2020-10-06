20ad2a85c77d6c46716b069ad19d5e0bb3c8697f
Amesh Alex Kuruvilla <64385937+ameshalexk@users.noreply.github.com>
2020-10-06 12:12:08 -0400

    Update salesbymatch.js

--- challenges/salesbymatch.js
+++ challenges/salesbymatch.js
@@ -1,11 +1,56 @@
-const arr = [10,20,20,10,10,30,50,10,20];
-let count = []; 
+'use strict';
 
-for (let i = 0; i <= arr.length; i++) {
-    if (arr.includes(arr[i])) {
-        count.push(arr[i])
-    }
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
-console.log(count);
 
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let count = 0;
+    const set = new Set();
 
+    for (let i = 0; i < ar.length; i++) {
+      if (set.delete(ar[i])) {
+        count++;
+      } else {
+        set.add(ar[i])
+      }
+    }
+
+    return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
