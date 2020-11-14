c9522d8b690ee2c9ffbfc279f5d51a15ba23e7da
Amesh Alex Kuruvilla <64385937+ameshalexk@users.noreply.github.com>
2020-11-13 22:11:57 -0500

    Create palindrome.js

---
+++ challenges/palindrome.js
@@ -0,0 +1,17 @@
+def is_palindrome(word)
+  reversed = ""
+
+  i = 0
+  while i < word.length
+    char = word[i]
+    reversed = char + reversed
+
+    i += 1
+  end
+
+  return reversed == word
+end
+
+puts is_palindrome("racecar")  # => true
+puts is_palindrome("kayak")    # => true
+puts is_palindrome("bootcamp") # => false
