2aac8a8c0cea3f3654e6f0e6f083e223b6dd30ee
Amesh Alex Kuruvilla <64385937+ameshalexk@users.noreply.github.com>
2020-11-13 22:35:16 -0500

    Update palindrome.js

--- challenges/palindrome.js
+++ challenges/palindrome.js
@@ -15,3 +15,13 @@ end
 puts is_palindrome("racecar")  # => true
 puts is_palindrome("kayak")    # => true
 puts is_palindrome("bootcamp") # => false
+
+
+////
+
+
+function checkPalindrome(inputString) {
+    return inputString === inputString.split('').reverse().join('');
+      
+  }
+  
