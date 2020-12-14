1f1cfc054f54957f110752be0fc77293ea521e48
ameshalexk <ameshalexusa@gmail.com>
2020-12-14 15:22:20 -0600

    practise

--- challenges/slidingwindow.js
+++ challenges/slidingwindow.js
@@ -1,23 +1,23 @@
-function maxSubarraySum(arr, num) {
-  let maxSum = 0;
-  let tempSum = 0;
-  if (arr.length < num) return null;
-  for (let i = 0; i < num; i++) {
-    maxSum += arr[i];
-    // console.log(maxSum);
-  }
-  tempSum = maxSum;
-  console.log(tempSum, true);
-  for (let i = num; i < arr.length; i++) {
-    console.log(tempSum);
-    tempSum = tempSum - arr[i - num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
-    // console.log(tempSum, maxSum);
-  }
-  return maxSum;
-}
+// function maxSubarraySum(arr, num) {
+//   let maxSum = 0;
+//   let tempSum = 0;
+//   if (arr.length < num) return null;
+//   for (let i = 0; i < num; i++) {
+//     maxSum += arr[i];
+//     // console.log(maxSum);
+//   }
+//   tempSum = maxSum;
+//   console.log(tempSum, true);
+//   for (let i = num; i < arr.length; i++) {
+//     console.log(tempSum);
+//     tempSum = tempSum - arr[i - num] + arr[i];
+//     maxSum = Math.max(maxSum, tempSum);
+//     // console.log(tempSum, maxSum);
+//   }
+//   return maxSum;
+// }
 
-console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+// console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
 
 // Naive solution
 // function maxSubarraySum(arr, num) {
@@ -39,12 +39,11 @@ console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
 
 // maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
 
-
 function findLongestSubstring(str) {
   let longest = 0;
   let seen = {};
   let start = 0;
- 
+
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
     if (seen[char]) {
@@ -57,3 +56,5 @@ function findLongestSubstring(str) {
   }
   return longest;
 }
+
+console.log(findLongestSubstring('ameshalex'));
