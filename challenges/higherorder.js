0ec9286d28757c489ecc0c6d2d25648019433752
ameshalex <ameshalexusa@gmail.com>
2020-12-16 13:15:35 -0500

    edited higherorder

--- challenges/higherorder.js
+++ challenges/higherorder.js
@@ -1,9 +1,24 @@
-function multiplyBy(num) {
-  return function (x) {
-    return x * num;
+// function multiplyBy(num) {
+//   return function (x) {
+//     return x * num;
+//   }
+// }
+
+// const triple = multiplyBy(2)
+
+// triple(6)
+
+
+//factory
+
+function makeBetweenFunc(x,y) {
+  return function (num) {
+    return num>= x && num <= y;
   }
 }
 
-const triple = multiplyBy(2)
 
-triple(6)
+const isChild  = makeBetweenFunc(0,18);
+
+
+console.log(isChild(77));
