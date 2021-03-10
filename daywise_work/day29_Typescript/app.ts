8a799767130d4df18b1ff821cb023a68de2ae882
ameshalexk <ameshalexusa@gmail.com>
2021-03-10 10:48:41 -0600

    wip day29 continuation

--- daywise_work/day29_Typescript/app.ts
+++ daywise_work/day29_Typescript/app.ts
@@ -1,1 +1,9 @@
-console.log("sdd");
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 4;
+
+const result = add(number1, number2);
+console.log(result);
