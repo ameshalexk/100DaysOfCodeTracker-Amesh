d2b22e816339cda2cc3e1c05f929b1c82ba55eff
ameshalexk <ameshalexusa@gmail.com>
2021-03-29 16:43:36 -0500

    made class timer

---
+++ daywise_work/day32_JS_Timer_Prac/index.js
@@ -0,0 +1,18 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.startButton.addEventListener('click', this.start);
+  }
+
+  start() {
+    console.log('Start');
+  }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
