335887291851c60a0adca4c5931d8ded28341dc0
ameshalexk <ameshalexusa@gmail.com>
2021-03-30 15:47:31 -0500

    timer value set to 30 and timer works

--- daywise_work/day32_JS_Timer_Prac/index.js
+++ daywise_work/day32_JS_Timer_Prac/index.js
@@ -17,7 +17,8 @@ class Timer {
   };
 
   tick = () => {
-    console.log('tick');
+    const timeRemaining = parseFloat(this.durationInput.value);
+    this.durationInput.value = timeRemaining - 1;
   };
 }
 
