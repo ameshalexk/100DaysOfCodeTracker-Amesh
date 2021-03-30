422d387a91792f0a9a4ff8cdd84ca8c183702b00
ameshalexk <ameshalexusa@gmail.com>
2021-03-30 17:46:29 -0500

    added functionality for animation

--- daywise_work/day32_JS_Timer_Prac/index.js
+++ daywise_work/day32_JS_Timer_Prac/index.js
@@ -1,61 +1,21 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callback) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-    if (callback) {
-      this.onStart = callback.onStart;
-      this.onTick = callback.onTick;
-      this.onComplete = callback.onComplete;
-    }
-  }
-
-  start = () => {
-    if (this.onStart) {
-      this.onStart();
-    }
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-      if (this.onComplete) {
-        this.onComplete();
-      }
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-      if (this.onTick) {
-        this.onTick();
-      }
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('Timer Started');
   },
   onTick() {
-    console.log('Tick Tock');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = currentOffset - 50;
   },
   onComplete() {
     console.log('Completed');
---
+++ daywise_work/day32_JS_Timer_Prac/timer.js
@@ -0,0 +1,47 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callback) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+    if (callback) {
+      this.onStart = callback.onStart;
+      this.onTick = callback.onTick;
+      this.onComplete = callback.onComplete;
+    }
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
