76ef0e3e1de34e6be093e461bdc6b8da8bc75a4e
ameshalexk <ameshalexusa@gmail.com>
2021-03-30 16:49:34 -0500

    added onStart, OnTick and OnComplete functionality

--- daywise_work/day32_JS_Timer_Prac/index.js
+++ daywise_work/day32_JS_Timer_Prac/index.js
@@ -1,13 +1,21 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callback) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
+    if (callback) {
+      this.onStart = callback.onStart;
+      this.onTick = callback.onTick;
+      this.onComplete = callback.onComplete;
+    }
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     this.interval = setInterval(this.tick, 1000);
   };
@@ -19,8 +27,14 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
     }
   };
 
@@ -36,4 +50,14 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer Started');
+  },
+  onTick() {
+    console.log('Tick Tock');
+  },
+  onComplete() {
+    console.log('Completed');
+  },
+});
