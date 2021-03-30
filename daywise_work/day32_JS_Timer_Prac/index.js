cf948d2f8b2058ab601299a93e5bd5222b4d8140
ameshalexk <ameshalexusa@gmail.com>
2021-03-30 15:16:43 -0500

    this fix with bind

--- daywise_work/day32_JS_Timer_Prac/index.js
+++ daywise_work/day32_JS_Timer_Prac/index.js
@@ -3,12 +3,12 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-    this.startButton.addEventListener('click', this.start);
+    this.startButton.addEventListener('click', this.start.bind(this));
   }
 
-  start = () => {
+  start() {
     this.importantMethod();
-  };
+  }
 
   importantMethod() {
     console.log('This is Important');
@@ -20,4 +20,3 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
-// timer.start();
