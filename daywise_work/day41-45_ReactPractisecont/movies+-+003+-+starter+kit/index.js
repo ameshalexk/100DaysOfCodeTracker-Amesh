d4d42ffc65bb24af8f0bc51fbe4a0a1cdc349740
Amesh Alex Kuruvilla <amesh@Ameshs-Mac-mini.local>
2021-04-13 16:38:07 -0500

    debounce functionality added to api call

--- daywise_work/day41-45_ReactPractisecont/movies+-+003+-+starter+kit/index.js
+++ daywise_work/day41-45_ReactPractisecont/movies+-+003+-+starter+kit/index.js
@@ -1,1 +1,24 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'f7bf45fe',
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+let timeoutId;
+
+const onInput = (e) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(e.target.value);
+  }, 1000);
+};
+
+input.addEventListener('input', onInput);
