88cdba1a42448b456a2c9f0a1febb1d20969c1f0
Amesh Alex Kuruvilla <amesh@Ameshs-Mac-mini.local>
2021-04-16 00:00:22 -0500

    custom textinput made

--- daywise_work/day46-55_RN_Prac/rn-first-app/App.js
+++ daywise_work/day46-55_RN_Prac/rn-first-app/App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 10,
+            width: '80%',
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
