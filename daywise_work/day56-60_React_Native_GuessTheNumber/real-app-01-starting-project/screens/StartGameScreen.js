c6b35db5a6fda08cf33e40ccdd3a88e03fedc5e7
Amesh Alex Kuruvilla <amesh@Ameshs-Mac-mini.local>
2021-04-19 14:43:46 -0500

    moved card logic to separate component

--- daywise_work/day56-60_React_Native_GuessTheNumber/real-app-01-starting-project/screens/StartGameScreen.js
+++ daywise_work/day56-60_React_Native_GuessTheNumber/real-app-01-starting-project/screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import Card from '../components/Card';
 
 function StartGameScreen() {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start A New Game! </Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 }
@@ -31,14 +32,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 0,
-    shadowOpacity: 0.26,
-    backgroundColor: 'white',
-    elevation: 8,
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
