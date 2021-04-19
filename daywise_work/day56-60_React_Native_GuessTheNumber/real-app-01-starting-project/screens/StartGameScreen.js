bc279ce41bad7b9261db0d24e4fc7362bb07a772
Amesh Alex Kuruvilla <amesh@Ameshs-Mac-mini.local>
2021-04-19 15:12:55 -0500

    made Input Component and styled components

--- daywise_work/day56-60_React_Native_GuessTheNumber/real-app-01-starting-project/screens/StartGameScreen.js
+++ daywise_work/day56-60_React_Native_GuessTheNumber/real-app-01-starting-project/screens/StartGameScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import Card from '../components/Card';
+import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 function StartGameScreen() {
   return (
@@ -8,10 +10,27 @@ function StartGameScreen() {
       <Text style={styles.title}>Start A New Game! </Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+        />
         <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
+          <Button
+            style={styles.button}
+            title="Reset"
+            onPress={() => {}}
+            color={Colors.accent}
+          />
+          <Button
+            style={styles.button}
+            title="Confirm"
+            onPress={() => {}}
+            color={Colors.primary}
+          />
         </View>
       </Card>
     </View>
@@ -38,6 +57,13 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'space-between',
     paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: 'center',
   },
 });
 export default StartGameScreen;
