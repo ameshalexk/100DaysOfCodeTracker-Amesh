54bd42e1cdb13e8d4491eb0169a6156249968422
Amesh Alex Kuruvilla <amesh@Ameshs-Mac-mini.local>
2021-04-19 11:07:08 -0500

    modal added

--- daywise_work/day46-55_RN_Prac/rn-first-app/components/GoalInput.js
+++ daywise_work/day46-55_RN_Prac/rn-first-app/components/GoalInput.js
@@ -8,6 +8,11 @@ const GoalInput = (props) => {
     setEnteredGoal(enteredText);
   };
 
+  const addGoalHandler = (goal) => {
+    props.onAddGoal(enteredGoal);
+    setEnteredGoal('');
+  };
+
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
@@ -17,7 +22,14 @@ const GoalInput = (props) => {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="ADD" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="CANCEL" onPress={props.onCancel} color="red" />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -35,6 +47,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+  },
+  button: {
+    width: '40%',
   },
 });
 
