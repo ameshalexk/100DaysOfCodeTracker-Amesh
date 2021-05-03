e59ad586c27efa299b57ba6e334c12cdee5f8471
Amesh Alex Kuruvilla <amesh@Ameshs-Mac-mini.local>
2021-05-03 15:56:38 -0500

    contract linked and tested

--- daywise_work/day62_ethereumproj/inbox/test/Inbox.test.js
+++ daywise_work/day62_ethereumproj/inbox/test/Inbox.test.js
@@ -2,29 +2,23 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-class Car {
-  park() {
-    return 'stopped';
-  }
+let accounts;
+let inbox;
 
-  drive() {
-    return 'vroom';
-  }
-}
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  //Use one of those accounts to deploy the contract
 
-let car;
-
-beforeEach(() => {
-  car = new Car();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi There!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
-describe('Car Class Testing', () => {
-  it('park testing', () => {
-    assert.equal(car.park(), 'stopped');
-  });
-
-  it('driver testing', () => {
-    assert.equal(car.drive(), 'vroom');
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(inbox);
   });
 });
