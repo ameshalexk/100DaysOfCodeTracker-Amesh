f5e92ce88b9b9a812c19721c3a6311f050290cfb
ameshalexk <ameshalexusa@gmail.com>
2021-05-12 00:08:18 -0500

    enter lottery logic added

--- daywise_work/day65-70_EthereumLotteryproj/ballot.sol
+++ daywise_work/day65-70_EthereumLotteryproj/ballot.sol
@@ -2,9 +2,13 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
     
     function Lottery() public {
         manager = msg.sender;
     }
-    
-}
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+}   
