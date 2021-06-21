820631ad7234045ee6b08427dc5c7a82065ca149
ameshalexk <ameshalexusa@gmail.com>
2021-06-21 00:00:16 -0500

    v5

--- daywise_work/day81_Linkedin/linkedinSnippetv5.js
+++ daywise_work/day81_Linkedin/linkedinSnippetv5.js
@@ -7,6 +7,8 @@
 // @match        https://www.linkedin.com/jobs/search/*
 // @icon         https://www.google.com/s2/favicons?domain=linkedin.com
 // @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.2/axios.min.js
+
 // ==/UserScript==
 
 (function () {
@@ -17,8 +19,8 @@
   if (!window.location.search.includes('f_E')) {
     newUrl = `https://www.linkedin.com/jobs/search/?f_E=1%2C2%2C3&f_TPR=r604800&geoId=103644278&keywords=${keywords}`;
     window.location.href = newUrl;
-    console.log(window.location.href);
-    console.log(newUrl, 'sdsd');
+    // console.log(window.location.href);
+    // console.log(newUrl, 'sdsd');
   }
 
   let objDiv = document.querySelector('.jobs-search-results');
@@ -31,18 +33,17 @@
     scroll = setInterval(function () {
       objDiv.scrollBy(0, getRandomArbitrary(501, 602));
       objDiv.scrollBy(0, getRandomArbitrary(51, 91));
-      console.log('start');
+      // console.log('start');
     }, getRandomArbitrary(1351, 1502));
     // return scroll;
   }
 
   function stop_scroll_down() {
     clearInterval(scroll);
-    console.log('stop');
+    // console.log('stop');
   }
 
   start_scroll_down();
-
   setTimeout(function () {
     stop_scroll_down();
   }, getRandomArbitrary(7355, 7521));
@@ -76,10 +77,46 @@
       arr.style.cssText = 'display: none';
       return arr;
     });
-    console.log('done');
+    // console.log('done');
     objDiv.scrollTo(0, 0);
 
-    let colorJobs = document.querySelectorAll('.jobs-search-results__list');
+    let colorJobs = document.querySelectorAll(
+      '.jobs-search-results__list-item'
+    );
     let finixArray = Array.from(colorJobs);
+    // console.log(finixArray);
+    let amesh = [];
+
+    finixArray.filter((e) => {
+      if (e.style.display != 'none') {
+        let needed = e.innerText.split('\n');
+        return amesh.push([needed[0], needed[1], needed[2]]);
+      }
+    });
+    // console.log(amesh, 'fffff');
+
+    let csvContent = 'data:text/csv;charset=utf-8,';
+
+    amesh.forEach(function (rowArray) {
+      let row = rowArray.join(',');
+      csvContent += row + '\r\n';
+    });
+    var encodedUri = encodeURI(csvContent);
+    console.log(encodedUri);
+    // const options = {
+    //   method: 'post',
+    //   url: 'http://ameshportfolio.herokuapp.com/',
+    //   data: encodedUri,
+    //   transformRequest: [
+    //     (data, headers) => {
+    //       // transform the data
+
+    //       return data;
+    //     },
+    //   ],
+    // };
+
+    // // send the request
+    // axios(options);
   }, getRandomArbitrary(8997, 9215));
 })();
